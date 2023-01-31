def312bc22e7bb664c85ca09bb7b235f2acf90ba
Radiance Woodruff <120369396+monteerose@users.noreply.github.com>
2023-01-30 21:45:16 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let count = 0
+let count1 = 0
+let count2 = 0
+
+function add() {
+    var counting = document.querySelector("#Neil");
+    count++;
+    counting.innerText = + count;   
+}
+
+
+
+function plus() {
+    var counter = document.querySelector("#Nikki")
+    count1++;
+    counter.innerText = + count1;     
+}
+
+function more() {
+    var ugh = document.querySelector("#Jim");
+    count2++;
+    ugh.innerText = + count2
+}
+
